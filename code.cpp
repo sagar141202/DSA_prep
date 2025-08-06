198e97189c78abafc5258d150b5ff46ed90da240
Sagar Maddi <sagarmaddi@Sagars-MacBook-Air.local>
2025-08-06 12:39:42 +0530

    Initial commit: DSA C++ code

---
+++ code.cpp
@@ -0,0 +1,8 @@
+#include<iostream> //Preprocessor directive to include the iostream header file
+using namespace std; //Using the standard namespace
+
+int main()
+{
+    cout << "Hello, World!" << endl; //Outputting "Hello, World!" to the console
+    return 0; //Returning 0 to indicate successful execution
+}
