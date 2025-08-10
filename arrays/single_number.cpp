ced278c0c0198109c43b7455aff03128d0fa97b1
Sagar Maddi <msagar111402@gmail.com>
2025-08-10 16:25:46 +0530

    Single number

---
+++ arrays/single_number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n = nums.size();
+        int result = 0;
+
+        for(int num : nums)
+        {
+            result ^= num;
+        }
+
+        return result;
+    }
+};
