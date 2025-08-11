966d8b2e8a52f33d7dee14c43e6f7bbcbb1755dc
Sagar Maddi <msagar111402@gmail.com>
2025-08-11 16:14:50 +0530

    Best time to buy and sell stock

---
+++ arrays/stock_buy_and_sell.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for(int i = 1; i < n; i++)
+        {
+            if(prices[i] < minPrice)
+            {
+                minPrice = prices[i];
+            }
+
+            else
+            {
+                maxProfit = max(maxProfit, prices[i] - minPrice);
+            }
+        }
+
+        return maxProfit;
+    }
+};
