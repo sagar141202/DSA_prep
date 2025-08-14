fe1c10c2fe5656c8856de7d88a78bf0afd8aea28
Sagar Maddi <msagar111402@gmail.com>
2025-08-14 13:04:17 +0530

    Count Inversions

---
+++ arrays/count_inversions.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+   long long int numberOfInversions(vector<int> nums) {
+        int n = nums.size();
+
+        int count = 0;
+
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = i + 1; j < n; j++)
+            {
+                if(nums[i] > nums[j])
+                {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+};
