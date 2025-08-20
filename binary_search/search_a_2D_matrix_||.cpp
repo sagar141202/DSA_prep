b6c4f874f70e18859f3225f7d901ab47794493fc
Sagar Maddi <msagar111402@gmail.com>
2025-08-20 19:00:23 +0530

    Search a 2D Matrix ||

---
+++ binary_search/search_a_2D_matrix_||.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        int row = 0;
+        int col = m - 1;
+
+        while(row < n && col >= 0 )
+        {
+            int val = matrix[row][col];
+
+            if(val == target)
+            {
+                return true;
+            }
+
+            else if(val > target)
+            {
+                col--;
+            }
+
+            else
+            {
+                row++;
+            }
+        }
+        return false;
+    }
+};
